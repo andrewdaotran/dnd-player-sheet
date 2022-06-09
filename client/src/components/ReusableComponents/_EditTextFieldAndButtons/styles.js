7e0b26a9760ec56618868a66014952d7605d2f10
Andrew Tran <andrewdaotran@gmail.com>
2022-06-08 17:53:59 -0700

    Fix _EditTextFieldAndButtons back button styling

--- client/src/components/ReusableComponents/_EditTextFieldAndButtons/styles.js
+++ client/src/components/ReusableComponents/_EditTextFieldAndButtons/styles.js
@@ -14,4 +14,8 @@ export const submitButton = {
 
 export const cancelButton = {
 	margin: '1rem 0 0 1rem',
+	backgroundColor: themeColors.royalRed,
+	':hover': {
+		backgroundColor: themeColors.lighterRoyalRed,
+	},
 }
